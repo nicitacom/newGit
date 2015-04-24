cc5653b9c47417ee7bb424127d44db11f0baf5a2
Никита <user@MacBook-Pro-user.local>
2015-04-24 22:29:57 +0300

    Initial Commit

---
+++ NewGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewGit
+//
+//  Created by Никита on 24.04.15.
+//  Copyright (c) 2015 Nikita Minakov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewGit
+//
+//  Created by Никита on 24.04.15.
+//  Copyright (c) 2015 Nikita Minakov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
